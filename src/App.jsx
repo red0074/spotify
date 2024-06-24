8ebbcbd24316cb48f3e882f88c2820de3987c0d5
red0074 <pdinesh122003@gmail.com>
2024-06-24 22:01:37 +0530

    login authentication with spotify

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./components/pages/Login";
+import { getTokenFromUrl } from "./spotify";
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+    }
+    console.log("i have token : ", token);
+  }, []);
   return (
     <>
       {/* login page as landing page */}
-      <Login />
+      {/* <Login /> */}
+      {/* we use the ternary operator if there is token then player page or else login page */}
+      {token ? <h1>i loggged in</h1> : <Login />}
     </>
   );
 }
