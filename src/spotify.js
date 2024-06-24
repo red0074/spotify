d6b0b2b8ef5c2e989df12bd726f5ef2c440c9c69
red0074 <pdinesh122003@gmail.com>
2024-06-24 21:59:59 +0530

    corrected the code based on latest chnages in spotify developer documentation

--- src/spotify.js
+++ src/spotify.js
@@ -21,15 +21,11 @@ export const getTokenFromUrl = () => {
   return window.location.hash
     .substring(1)
     .split("&")
-    .reduce(
-      initial,
-      (item) => {
-        let parts = item.split("=");
-        initial[parts[0]] = decodeURIComponent(parts[1]);
-        return initial;
-      },
-      {}
-    );
+    .reduce((initial, item) => {
+      let parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+      return initial;
+    }, {});
 };
 
 //url
