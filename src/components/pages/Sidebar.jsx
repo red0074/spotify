7eaeebee28c6f1d0b07eb2882f4b78fc29da21d1
red0074 <pdinesh122003@gmail.com>
2024-06-26 19:03:56 +0530

    created the sidebar panel

---
+++ src/components/pages/Sidebar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import LibraryMusicOutlinedIcon from "@mui/icons-material/LibraryMusicOutlined";
+import { useDataLayerValue } from "./DataLayer";
+
+function Sidebar() {
+  const [{ playlists }, dispatch] = useDataLayerValue();
+  return (
+    <div className="sidebar">
+      <img
+        className="sidebar__logo"
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt="spotify-logo"
+      />
+      {/* options */}
+      <SidebarOption Icon={HomeOutlinedIcon} title="Home" />
+      <SidebarOption Icon={SearchOutlinedIcon} title="Search" />
+      <SidebarOption Icon={LibraryMusicOutlinedIcon} title="Your Library" />
+      <br />
+      <strong className="sidebar__title">PLAYLISTS</strong>
+      <hr />
+      {playlists?.items?.map((playlist) => (
+        <SidebarOption title={playlist.name} />
+      ))}
+    </div>
+  );
+}
+
+export default Sidebar;
