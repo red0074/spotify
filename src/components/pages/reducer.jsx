38826a842d5335a381fc925588a9a198ba587ebe
red0074 <pdinesh122003@gmail.com>
2024-06-26 12:46:17 +0530

    added the datalayer that takes the token and give the data from spotidy

---
+++ src/components/pages/reducer.jsx
@@ -0,0 +1,29 @@
+export const initialState = {
+  user: null,
+  playlists: [],
+  playing: false,
+  item: null,
+  // token: null,
+  token:
+    "BQAzApDf6ES6XJEvwmGmLZOSHKoV6zc0O--qXeg9qU_ZCsXF5Ow2BriqrZdTvNMPEkOm9eUDK3ksmAcm7-g3LFXLYdf4tcqBJ9boGwpghK9wekZNnzv0h4RNjLm64Tr-im_Bugk75ja0Extsx2ZJcTu3c6xGO57AFjgXC4qRXsz71wWWTYpZGq2GxsTTrqSfLZr9cpwwHv2t7pW4",
+};
+
+const reducer = (state, action) => {
+  console.log(action);
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
