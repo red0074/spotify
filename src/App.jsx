821428287869a4600f85a022174bbf408309d357
red0074 <pdinesh122003@gmail.com>
2024-06-26 12:47:43 +0530

    updated by adding the context api and wraped our app with datalayer

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,43 @@ import Login from "./components/pages/Login";
 import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
 import Player from "./components/pages/Player";
+import { useDataLayerValue } from "./components/pages/DataLayer";
 
 //instance of spotifwebapi
 const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState(null);
+  const [{ user, token }, dispatch] = useDataLayerValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
-    window.location.hash = "";
 
     const _token = hash.access_token;
     if (_token) {
-      setToken(_token);
+      // setToken(_token);
       //accessing using token endpoint(interaction btw spotify and react)
-      spotify.setAccessToken(_token);
+
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
+      });
     }
-    console.log("i have token : ", token);
   }, []);
+
   return (
     <>
       {/* login page as landing page */}
       {/* <Login /> */}
       {/* we use the ternary operator if there is token then player page or else login page */}
       {/* {token ? <h1>i loggged in</h1> : <Login />} */}
-      {token ? <Player /> : <Login />}
+      {token ? <Player spotify={spotify} /> : <Login />}
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { DataLayer } from "./components/pages/DataLayer";
+import initialState from "./components/pages/reducer.jsx";
+import reducer from "./components/pages/reducer.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <DataLayer initialState={initialState} reducer={reducer}>
+      <App />
+    </DataLayer>
+  </React.StrictMode>
+);
