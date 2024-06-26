513eda7ad814bbbaea00b34fae60f9c4cb3e5e49
red0074 <pdinesh122003@gmail.com>
2024-06-26 11:37:53 +0530

    installed spority web api and updated the ternary operation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./components/pages/Login";
 import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./components/pages/Player";
+
+//instance of spotifwebapi
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
@@ -13,6 +18,8 @@ function App() {
     const _token = hash.access_token;
     if (_token) {
       setToken(_token);
+      //accessing using token endpoint(interaction btw spotify and react)
+      spotify.setAccessToken(_token);
     }
     console.log("i have token : ", token);
   }, []);
@@ -21,7 +28,8 @@ function App() {
       {/* login page as landing page */}
       {/* <Login /> */}
       {/* we use the ternary operator if there is token then player page or else login page */}
-      {token ? <h1>i loggged in</h1> : <Login />}
+      {/* {token ? <h1>i loggged in</h1> : <Login />} */}
+      {token ? <Player /> : <Login />}
     </>
   );
 }
