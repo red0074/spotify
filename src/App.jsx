d18f1906a0a62818b472ec01e0ff343a5935682d
red0074 <pdinesh122003@gmail.com>
2024-06-27 12:41:51 +0530

    completed styling of the pages and rendered some songs and song covers

--- src/App.jsx
+++ src/App.jsx
@@ -39,6 +39,12 @@ function App() {
           playlists: playlists,
         });
       });
+      spotify.getPlaylist("37i9dQZEVXcL2rVZBxNaYk").then((response) => {
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        });
+      });
     }
   }, []);
 
